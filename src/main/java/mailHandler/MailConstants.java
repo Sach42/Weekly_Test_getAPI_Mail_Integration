788275080d588348bd94cd657516d6c3573aabec
Sachin Shukla <104413867+Sach42@users.noreply.github.com>
2023-09-10 15:54:37 +0530

    Update MailConstants.java

--- src/main/java/mailHandler/MailConstants.java
+++ src/main/java/mailHandler/MailConstants.java
@@ -2,5 +2,5 @@ package mailHandler;
 
 public class MailConstants {
     public static final String SENDER = "sachinkumar05282@gmail.com";
-    public static final String APP_PASSWORD = "tfuotbawqnaahcwx";
+    public static final String APP_PASSWORD = "";
 }
